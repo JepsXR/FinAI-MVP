ced3775f8ea5428d4c5e439eaeb19cd775f88c9a
JepsXR <juanes501lol@gmail.com>
2026-03-16 11:25:36 -0500

    Initial commit: FinAI backend core.

---
+++ main.py
@@ -0,0 +1,38 @@
+# Step 1: IMPORT
+
+from fastapi import FastAPI, HTTPException
+from typing import List, Optional
+from pydantic import BaseModel
+from dotenv import load_dotenv
+import google.generativeai as genai
+import os
+import sqlite3
+
+# Step 2: SERVER ACTIVATION
+
+app = FastAPI(
+    title="FinAI agent",
+    description="Fin it's a agent that explain financial terms to people in a simple way ",
+    version="1.0.0"
+)
+
+load_dotenv()
+
+keymaster = os.getenv("FINAI_API_KEY")
+if keymaster is None:
+    print("Ups....don't find api key")
+else:
+    genai.configure(api_key=keymaster)
+    print("Sucessfull connection")
+
+# Step 3: CONNECTION WITH A DATABASE
+
+connection = sqlite3.connect('database')
+cursor = connection.cursor()
+
+cursor.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)")
+cursor.execute("INSERT INTO users (name) VALUES ('Your name')")
+
+connection.commit()
+print("¡Data saved sucessfully!")
+connection.close()
